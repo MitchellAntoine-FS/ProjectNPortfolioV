9965703b2e2f83ace43c9f4184133f7beceb529e
MitchellAntoine-FS <amitchell3@student.fullsail.edu>
2023-03-06 19:47:10 -0800

    Initial commit

---
+++ MitchellAntoine_DankTank/app/src/main/java/com/mitchant/mitchellantoine_danktank/MainActivity.java
@@ -0,0 +1,13 @@
+package com.mitchant.mitchellantoine_danktank;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
